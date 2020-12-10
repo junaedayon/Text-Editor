e59b9ac26822cedf14d846187533e55fd12ca39c
Ayon <junaedayon2702@gmail.com>
2020-12-11 01:26:50 +0600

    first commit

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,33 +1,34 @@
-const submit_text = document.getElementById('submit_text');
-const format_text = document.getElementById('format_text')
+const submi_text = document.getElementById("submit_text");
+const format_text =document.getElementById("format_text");
 
-function updateText() {
-    let text = submit_text.value;
-    format_text.innerText = text
-};
+function updateText () {
+   let text = submit_text.value;
+   format_text.innerText = text
+}
+submit_text.addEventListener('input' , updateText);
 
 function makeBold (elem) {
-       elem.classList.toggle('active');
-       document.getElementById('format_text').classList.toggle('bold');
+    elem.classList.toggle('active');
+
+    document.getElementById("format_text").classList.toggle('bold');
 }
+
 function makeItalic (elem) {
     elem.classList.toggle('active');
-    document.getElementById('format_text').classList.toggle('italic');
+    document.getElementById("format_text").classList.toggle('italic');
 }
 function makeUnderline (elem) {
     elem.classList.toggle('active');
-    document.getElementById('format_text').classList.toggle('underline');
+    document.getElementById("format_text").classList.toggle('underline');
 }
 
 function alignText (elem , alignType) {
-    // elem.classList.toggle('active') ; 
-
-    document.getElementById('format_text').style.textAlign = alignType;
-    let buttonList = document.getElementsByClassName('align');
-    for (let i = 0 ; i < buttonList.length ; i++) {
-
-        buttonList[i].classList.remove('active');
-    }
-    elem.classList.add('active');
+    // elem.classList.toggle('active');
+   document.getElementById("format_text").style.textAlign = alignType;
+     let buttonList = document.getElementsByClassName('align');
+     for (let i = 0 ; i < buttonList.length; i++) {
+         buttonList[i].classListremove('active');
+     }
+      elem.classList.add('active')
+        
 }
-submit_text.addEventListener('input', updateText);
