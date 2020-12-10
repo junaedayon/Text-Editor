cbed1dc9f44ea8fa43c94ce2a1cc2535fc70bd7c
Ayon <junaedayon2702@gmail.com>
2020-12-10 22:59:28 +0600

    first commit

---
+++ assets/js/home.js
@@ -0,0 +1,33 @@
+const submit_text = document.getElementById('submit_text');
+const format_text = document.getElementById('format_text')
+
+function updateText() {
+    let text = submit_text.value;
+    format_text.innerText = text
+};
+
+function makeBold (elem) {
+       elem.classList.toggle('active');
+       document.getElementById('format_text').classList.toggle('bold');
+}
+function makeItalic (elem) {
+    elem.classList.toggle('active');
+    document.getElementById('format_text').classList.toggle('italic');
+}
+function makeUnderline (elem) {
+    elem.classList.toggle('active');
+    document.getElementById('format_text').classList.toggle('underline');
+}
+
+function alignText (elem , alignType) {
+    // elem.classList.toggle('active') ; 
+
+    document.getElementById('format_text').style.textAlign = alignType;
+    let buttonList = document.getElementsByClassName('align');
+    for (let i = 0 ; i < buttonList.length ; i++) {
+
+        buttonList[i].classList.remove('active');
+    }
+    elem.classList.add('active');
+}
+submit_text.addEventListener('input', updateText);
